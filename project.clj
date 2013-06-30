5603d9aca895db9154c23d1bdd90d5e084a5b6e4
Adam Solove <asolove@gmail.com>
2013-06-30 15:09:22 -0400

    Sketching beginnings of constraint relaxation api.

--- project.clj
+++ project.clj
@@ -4,12 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1450"]
-                 [com.keminglabs/cassowary "0.1.0"]]
+                 [org.clojure/clojurescript "0.0-1835"]
+                 [com.keminglabs/cassowary "0.1.0"]
+                 [com.cemerick/piggieback "0.0.5"]]
 
   :source-paths ["src/clj"]
 
   :plugins [[lein-cljsbuild "0.3.0"]]
+
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
   :cljsbuild {
               :builds [{
