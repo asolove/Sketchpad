069bf023898f2ae1517ab84e17658670281ba83d
Adam Solove <asolove@gmail.com>
2024-02-17 08:42:54 -0500

    Move line calculation to Rust. Remove twinkle for now.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-use std::fmt;
+use std::{char::MAX, fmt};
 use wasm_bindgen::prelude::*;
 
 extern crate web_sys;
@@ -14,37 +14,68 @@ macro_rules! log {
 }
 
 #[wasm_bindgen]
-pub struct Point {
-    x: i32,
-    y: i32,
+pub struct Pixel {
+    x: u16,
+    y: u16,
 }
 
 #[wasm_bindgen]
 pub struct Controller {
-    width: i32,
-    height: i32,
-    points: Vec<Point>,
+    pixels: Vec<Pixel>,
 }
 
 #[wasm_bindgen]
 impl Controller {
-    pub fn new(width: i32, height: i32) -> Controller {
+    pub fn new() -> Controller {
         utils::set_panic_hook();
-        let points: Vec<Point> = vec![Point { x: 1, y: 2 }];
+        let pixels: Vec<Pixel> = vec![Pixel { x: 1, y: 2 }];
 
-        Controller {
-            width,
-            height,
-            points,
-        }
+        Controller { pixels }
     }
 
-    pub fn mouse_moved(&self, x: i32, y: i32) {
+    pub fn pixels(&self) -> *const Pixel {
+        self.pixels.as_ptr()
+    }
+
+    pub fn pixels_size(&self) -> usize {
+        self.pixels.len()
+    }
+
+    pub fn mouse_moved(&self, x: u16, y: u16) {
         log!("Mouse moved: {}, {}", x, y);
     }
 
-    pub fn clicked(&mut self, x: i32, y: i32) {
+    pub fn clicked(&mut self, x: u16, y: u16) {
         log!("Clicked: {}, {}", x, y);
-        self.points.push(Point { x: x, y: y })
+        self.pixels.push(Pixel { x: x, y: y })
+    }
+
+    pub fn show_ink(&mut self) {
+        self.pixels = vec![];
+        let TOP = 200;
+        let BOTTOM = 800;
+        let LEFT = 100;
+        let RIGHT = 900;
+        self.show_line(LEFT, TOP, RIGHT, TOP);
+        self.show_line(RIGHT, TOP, RIGHT, BOTTOM);
+        self.show_line(RIGHT, BOTTOM, LEFT, BOTTOM);
+        self.show_line(LEFT, BOTTOM, LEFT, TOP);
+    }
+
+    pub fn show_line(&mut self, x1: u16, y1: u16, x2: u16, y2: u16) {
+        let xdiff = x2.abs_diff(x1);
+        let ydiff = y2.abs_diff(y1);
+        let steps = xdiff.max(ydiff);
+
+        let dx = xdiff as f64 / steps as f64;
+        let dy = ydiff as f64 / steps as f64;
+
+        let mut x = x2.min(x1);
+        let mut y: u16 = y2.min(y1);
+        for step in 0..steps {
+            self.pixels.push(Pixel { x, y });
+            x = (x as f64 + dx).round() as u16;
+            y = (y as f64 + dy).round() as u16;
+        }
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
-import { Controller } from "sketchpad";
+import { Controller, Pixel } from "sketchpad";
 import { memory } from "sketchpad/sketchpad_bg";
 
 const WIDTH = 1024;
@@ -8,49 +8,23 @@ canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
 let controller = Controller.new();
-
-
-let pixelsToDraw = [];
-let LEFT = 100;
-let RIGHT = 900;
-let TOP = 200;
-let BOTTOM = 800;
-for(let x=LEFT; x<RIGHT; x++) {
-  pixelsToDraw.push([x, TOP]);
-}
-for(let y=TOP; y<BOTTOM; y++) {
-  pixelsToDraw.push([RIGHT, y]);
-}
-for(let x=LEFT; x<RIGHT; x++) {
-  pixelsToDraw.push([x, BOTTOM]);
-}
-for(let y=BOTTOM; y>TOP; y--) {
-  pixelsToDraw.push([LEFT, y]);
-}
-
-let randomize = () => {
-  for(let i=0; i<pixelsToDraw.length; i++) {
-    let j = (i + Math.random() * pixelsToDraw.length) % pixelsToDraw.length;
-    let p = pixelsToDraw[j];
-    pixelsToDraw[j] = pixelsToDraw[i];
-    pixelsToDraw[i] = p;
-  }
-}
-
+controller.show_ink();
 
 let i = 0;
-let PIXELS_PER_DRAW = 100000 / 10 / 60;
+let PIXELS_PER_DRAW = 100000 / 20 / 60;
 
 let render = () => {
+  const pixels = new Uint16Array(memory.buffer, controller.pixels(), 2 * controller.pixels_size());
+
   let ctx = canvas.getContext('2d');
-  ctx.fillStyle = "rgb(0 0 0 / 10%)";
+  ctx.fillStyle = "rgb(0 0 0 / 3%)";
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
   ctx.fillStyle = "rgb(230 240 255 / 50%)";
-  for(let j=0; j<PIXELS_PER_DRAW; j++) {
-    i++;
+  for(let j=0; j<controller.pixels_size(); j++) {
     ctx.beginPath();
-    let pixel = pixelsToDraw[i % pixelsToDraw.length];
-    ctx.arc(pixel[0], pixel[1], 1, 0, 2 * Math.PI);
+    let xIndex = j * 2;
+    ctx.arc(pixels[xIndex], pixels[xIndex + 1], 1, 0, 2 * Math.PI);
     ctx.fill();
   }
 }
