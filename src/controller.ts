ac06a5098c19505402060e14683dbd02be621abb
Adam Solove <asolove@gmail.com>
2024-08-05 07:12:15 -0400

    Controller can create new picture.

--- src/controller.ts
+++ src/controller.ts
@@ -2,30 +2,47 @@ import type { Universe } from "./document";
 
 export class Controller {
   universe: Universe;
+  pictureCount: number;
 
   container: Element;
   select: HTMLSelectElement;
 
   constructor(container: Element, universe: Universe) {
     this.universe = universe;
+    this.pictureCount = 0;
     this.container = container;
     this.select = document.createElement("select");
     this.container.appendChild(this.select);
     this.select.addEventListener("change", (e) => {
-      let newCurrent = parseInt(e.currentTarget.value, 10);
-      this.universe.currentPicture = this.universe.pictures[newCurrent];
+      let target: HTMLSelectElement = e.currentTarget as any;
+      let newIndex = parseInt(target.value || "", 10);
+      if (this.universe.pictures[newIndex]) {
+        this.universe.currentPicture = this.universe.pictures[newIndex];
+      } else if (target.value === "New") {
+        debugger;
+        this.universe.addPicture();
+      }
     });
 
+    this.loop();
+  }
+
+  loop() {
     this.render();
+    requestAnimationFrame(() => this.loop());
   }
 
   render() {
     let pictureCount = this.universe.pictures.length;
+    if (pictureCount === this.pictureCount) return;
+    this.pictureCount = pictureCount;
+
     let options = "";
     for (let i = 0; i < pictureCount; i++) {
       let selected = this.universe.currentPicture == this.universe.pictures[i];
       options += `<option ${selected ? "selected" : ""}>${i}</option>`;
     }
+    options += "<option>New</option>";
     this.select.innerHTML = options;
   }
 }
